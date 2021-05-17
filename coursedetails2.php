d1a4864582a9b33bdb36fc8bf7227faf60e8e389
oishipaul235 <71022341+oishipaul235@users.noreply.github.com>
2021-05-17 18:53:08 +0530

    Add files via upload

---
+++ coursedetails2.php
@@ -0,0 +1,83 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Course details Page</title>
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+    <!-- <link rel="stylesheet" type="text/css" href="./css/home.css">  -->
+    <style>
+  
+    
+
+    .jumbotron {
+        background-position: top center;
+        background-size: cover;
+        height: 800px;
+        
+    }
+    .card{
+         background-color:honeydew;
+         width:1000px;
+         margin:10px 170px 100px 170px;
+         box-sizing:border-box;
+         box-shadow:10px 10px 10px black;
+     }
+     .img-style{
+         width:100%;
+         height:500px;
+     }
+     .container{
+        font-family:Goudy Stout;
+        padding:10px;
+        margin:10px 10px 10px 730px; 
+        background-color:Aquamarine;
+        width:600px;
+    }
+    </style>
+</head>
+
+<body>
+<?php
+include("./theme/header.php");
+?>
+    <section>
+    <div class="container"> <h1>COURSE DETAILS</h1></div>
+
+    <div class="jumbotron" style="background-image:url('./images/learn.jpg')">
+        </div>
+    </section>
+    <section class="course-card">
+        <div class="card">
+        <img class="img-style"src="./images/BHM.jpg">
+        <div class="card-body">
+      <h3> <div class="card-title"style="font-family:Franklin Gothic;text-align:center">BHM<br>BACHELOR OF HOSPITAL MANAGEMENT</div></h3>
+       <br> <div class="card-desc">
+        <P><h4 class="card-desc1"style="font-family:Arial Rounded MT">Duration: 3 YEARS<br><br>Price: INR 30,000</h4>
+        <br><br><h5 class="card-desc2"style="font-family:Comic Sans MS"><b><u>About Bachelor of Hotel Management (BHM):-</b></u></h5><br>
+    <h6 class="card-desc3"style="font-family:elephant">Bachelor of Hotel Management is an undergraduate program which imparts knowledge about various skills and fundamentals of hospitality and its different sectors. India is one of the most popular tourist destinations in the world. 
+    <br>There’s no doubt about the fact that hotel industry plays a crucial role in the hospitality industry. 
+    <br>Hence, there is a great demand for hotel management professionals who have studied about the hotel industry as a whole and have adequate knowledge about other essential factors such as hospitality, public relations, customer service, sales, marketing and others.
+<br>Students who are planning to pursue BHM must have good communication and organizational skills. 
+<br>Along with this, they should have a pleasing personality and an ability to interact with people and make them feel comfortable. BHM course is usually divided into six to eight semesters, but may vary from institute to institute. 
+<br><br><h5 class="card-desc2"style="font-family:Comic Sans MS"><b><u>Bachelor of Hotel Management (BHM) Eligibility Criteria:-</b></u></h5><br>
+    <h6 class="card-desc3"style="font-family:elephant"><ol>
+    <li>Minimum Qualification Required: 10+2</li>
+<li>Minimum Marks Required: 50% to 60%</li>
+<li>Age Requirement: The upper limit for the course is 22 years for the general category and 25 years for reserved category.</li>
+</ol>    <br><br><h5 class="card-desc2"style="font-family:Comic Sans MS"><b><u>BHM Job Prospects:-</b></u></h5><br>
+    <h6 class="card-desc3"style="font-family:elephant">BHM course exposes graduates to a pool of lucrative job opportunities.
+     <br>There are a number of employment areas and job positions in the market since there are various departments in the hotel industry.
+      <br>Mostly, at first, these graduates are hired for training and internships and then they get recruited for the positions that suit their personality at best.
+<br>Plenty of job opportunities are available for BHM graduates in various employment sectors correlated to hospitality and tourism. 
+<br>After completing this course, you can expect an average salary package up to INR 3-6 Lakhs per annum. 
+<br>With time and experience, you can also anticipate increment and job promotion.
+<br>BHM graduates can be recruited by a number of companies especially hotel chains and the tourism industry. </P></div>
+        </div>
+        </div>
+    </section>
+                        <?php
+include("./theme/footer.php");
+?>
+</body>
+
+</html>
---
+++ coursedetails3.php
@@ -0,0 +1,88 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Course details Page</title>
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+    <!-- <link rel="stylesheet" type="text/css" href="./css/home.css">  -->
+    <style>
+  
+    
+
+    .jumbotron {
+        background-position: top center;
+        background-size: cover;
+        height: 800px;
+       
+    }
+    .card{
+         background-color:lightcyan;
+         width:1000px;
+         margin:10px 170px 100px 170px;
+         box-sizing:border-box;
+         box-shadow:10px 10px 10px black;
+     }
+     .img-style{
+         width:100%;
+         height:500px;
+     }
+     .container{
+        font-family:Goudy Stout;
+        padding:10px;
+        margin:10px 10px 10px 730px; 
+        background-color:Aquamarine;
+        width:600px;
+    }
+    </style>
+</head>
+
+<body>
+<?php
+include("./theme/header.php");
+?>
+    <section>
+    <div class="container"> <h1>COURSE DETAILS</h1></div>
+
+    <div class="jumbotron" style="background-image:url('./images/learn.jpg')">
+        </div>
+    </section>
+    <section class="course-card">
+        <div class="card">
+        <img class="img-style"src="./images/bms.jpg">
+        <div class="card-body">
+      <h3> <div class="card-title"style="font-family:Franklin Gothic;text-align:center">BMS<br>BACHELOR OF MANAGEMENT STUDIES</div></h3>
+       <br> <div class="card-desc">
+        <P><h4 class="card-desc1"style="font-family:Arial Rounded MT">Duration: 3 YEARS<br><br>Price: INR 30,000</h4>
+        <br><br><h5 class="card-desc2"style="font-family:Comic Sans MS"><b><u>About Bachelor of Management Studies (BMS):-</b></u></h5><br>
+    <h6 class="card-desc3"style="font-family:elephant">Bachelor of Management Studies (BMS) is a three-year undergraduate program that offers advanced studies in the field of management that are necessary to run an organization efficiently.
+    <br> It also provides in-depth knowledge of human resource management, economics, and business studies.
+<br>The BMS course provides students with vast knowledge of changing world of management, new business techniques, entrepreneurship, trading, finance, stock and risk analysis. 
+<br>It includes various training sessions and extra and co-curricular activities that are essential for the overall development of the students to help them understand the real-world challenges. 
+<br>BMS also teaches budding entrepreneurs how to expand their businesses with least number of risks.
+<br>Students planning to undertake BMS must have team-building qualities and should be able to communicate effectively. 
+<br>They should also have knowledge about the current affairs and the stock market. 
+<br>Students learn to critically analyse situations and to solve the complexities of management.  </h6>
+  </h6>
+  <br><br><h5 class="card-desc2"style="font-family:Comic Sans MS"><b><u>The objective of the BMS course:-</b></u></h5><br>
+    <h6 class="card-desc3"style="font-family:elephant">In India, BMS is a UG course offered by top universities like IIT, University of Punjab, etc.
+    <br> The main focus is to create knowledge of human body structures and functions o from molecular to entire systems.
+ <br><br><h5 class="card-desc2"style="font-family:Comic Sans MS"><b><u>Bachelor of Management Studies (BMS) Career Options and Job Prospects:-</b></u></h5><br>
+    <h6 class="card-desc3"style="font-family:elephant">After pursuing a Bachelor of Management Studies (BMS) degree, students can explore different career opportunities while beginning their professional journey. 
+    <br>Interested students can go for higher studies and pursue master’s degree - ideally an MBA to further advance in their career. 
+    <br>The course provides students with an adequate understanding of management, making them fit for jobs in the areas such as Academic Institutions, Marketing & Sales, Finance & Retail, & Consulting etc.
+    <br> The average annual salary of a fresher is between Rs. 3 lakh to Rs. 4 lakh. <br>
+    Here’s a list of popular job roles for students with BMS degree:<br>
+<ol>
+<li>HR Executive</li>
+<li>Quality Manager</li>
+<li>Business Development Executive</li>
+<li>Assistant Manager</li></ol></P></div>
+        </div>
+        </div>
+    </section>
+                        <?php
+include("./theme/footer.php");
+?>
+</body>
+
+</html>
